1b8f8c08c88bdae53ed83788c30bb206bfcea67d
nikneym <halildrk@gmail.com>
2024-10-13 21:17:49 +0300

    chore: playground changes

--- playground/app.vue
+++ playground/app.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { SmoothActionDropdown } from "#components";
+import data from "./data.json";
 import type Column from "../src/runtime/types/column";
 
-const myData = ref<[]>([]);
+// @ts-expect-error
+const myData = ref<[]>([...data]);
 
 const id = ref<number>(1);
 
-const {
+/* const {
   data: swapiData,
   status,
   refresh,
@@ -20,7 +23,7 @@ const {
 
 const results = computed(() =>
   status.value === "success" ? swapiData.value!.results : []
-);
+); */
 
 const settings = {
   estimateSize: () => 56,
@@ -32,48 +35,86 @@ const settings = {
   height: 512,
 };
 
-const columns: Column[] = [
+const columns = ref<Column[]>([
+  {
+    title: "ID",
+    field: "id",
+    width: "200px",
+    // @ts-expect-error
+    format: (rowData) => rowData.user.id,
+  },
+  {
+    field: "avatar",
+    width: "54px",
+    // @ts-expect-error
+    format: (rowData) => rowData.user.avatar,
+    hide: false,
+  },
   {
     title: "Name",
     field: "name",
-    format: (rowData) => rowData.name,
-    //flex: "1",
     width: "200px",
+    // @ts-expect-error
+    format: (rowData) => rowData.user.first_name,
     search: {
       placeholder: "Search by name",
-      filter: (rowData, key, query) =>
-        rowData.name.toLowerCase().includes(query.toLowerCase()),
+      filter: (rowData: any, field: string, query: string) =>
+        rowData.user["first_name"].toLowerCase().includes(query.toLowerCase()),
     },
   },
   {
-    title: "Gender",
-    field: "gender",
-    format: (rowData) => rowData.gender,
+    title: "Surname",
+    field: "surname",
     width: "200px",
+
+    // @ts-expect-error
+    format: (rowData) => rowData.user.last_name,
+    search: {
+      placeholder: "Search by surname",
+      filter: (rowData: any, _, query: string) =>
+        rowData.user["last_name"].toLowerCase().includes(query.toLowerCase()),
+    },
   },
   {
-    title: "Height",
-    field: "height",
-    format: (rowData) => rowData.height,
+    title: "Place of Birth",
+    field: "place-of-birth",
     width: "200px",
+
+    // @ts-expect-error
+    format: (rowData) => rowData.info.place_of_birth,
+    selectable: {},
   },
   {
-    title: "Hair Color",
-    field: "hair-color",
-    format: (rowData) => rowData.hair_color,
+    title: "Age",
+    field: "age",
     width: "200px",
+
+    // @ts-expect-error
+    format: (rowData) => {
+      const date = new Date(rowData.user.date_of_birth);
+
+      return new Date().getFullYear() - date.getFullYear();
+    },
+    search: {
+      placeholder: "Search by surname",
+      filter: (rowData: any, _, query: string) =>
+        rowData.user["last_name"].toLowerCase().includes(query.toLowerCase()),
+    },
   },
+]);
+
+// actions array defines operations that user can do on rows
+const actions = [
   {
-    title: "Eye Color",
-    field: "eye-color",
-    format: (rowData) => rowData.eye_color,
-    width: "200px",
-  },
-  {
-    title: "Birth Year",
-    field: "birth-year",
-    format: (rowData) => rowData.birth_year,
-    width: "200px",
+    field: "action-menu",
+    render: SmoothActionDropdown,
+    options: [
+      { label: "Detayları Göster", action: () => console.log("details") },
+      { label: "İş Girişi", action: () => console.log("inputs") },
+      { label: "Vardiya Girişi", action: () => console.log("timings") },
+      { label: "Düzenle", action: () => console.log("edit") },
+      { label: "Sil", action: () => console.log("remove action") },
+    ],
   },
 ];
 
@@ -83,21 +124,25 @@ const table = ref();
 
 <template>
   <!-- controls -->
-  <button type="button" @click="results.splice(0, 1)">remove 1</button>
-  <button type="button" @click="swapiData.results = []">remove all</button>
+  <button type="button" @click="myData.splice(0, 1)">remove 1</button>
+  <button type="button" @click="myData = []">remove all</button>
   <!-- @vue-expect-error -->
-  <button type="button" @click="refresh">reload</button>
-  <button type="button" @click="id--">previous</button>
-  <button type="button" @click="id++">next</button>
+  <button type="button" @click="myData = [...data]">reload</button>
   <!-- table -->
   <div class="wrapper">
     <SmoothTable
       ref="table"
       :columns="columns"
+      :actions="actions"
       :settings="settings"
-      :data="results"
-      :status="status"
-    />
+      :data="myData"
+    >
+      <template #avatar="{ value }">
+        <div class="img-wrapper">
+          <img :src="value" loading="lazy" />
+        </div>
+      </template>
+    </SmoothTable>
   </div>
 </template>
 
